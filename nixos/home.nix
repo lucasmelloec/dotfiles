60860356a82a2f091f32712ee86f84188715b6a2
lucasmelloec <lucasmelloec@gmail.com>
2024-04-04 11:13:16 -0300

    wip: gpu troubles

--- nixos/home.nix
+++ nixos/home.nix
@@ -58,6 +58,7 @@
 
   xdg.configFile = {
     hypr = {
+      recursive = true;
       source = ../hypr/.config/hypr;
     };
     ags = {
--- nixos/machines/darkwings/configuration.nix
+++ nixos/machines/darkwings/configuration.nix
@@ -13,5 +13,12 @@
 
   # AMD GPU
   boot.initrd.kernelModules = [ "amdgpu" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.videoDrivers = lib.mkForce [ "amdgpu" ];
+
+  # Disable nvidia
+  boot.extraModprobeConfig = ''
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
+  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 }
